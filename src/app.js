3252103675a5fcd8d843210380a554b203c2f162
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-22 21:34:00 +0300

    add provider

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 
 import reducers from './reducers/index';
-import { addToCart } from './actions/cartActions';
+//import { addToCart } from './actions/cartActions';
 import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
 
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
-//store.subscribe(function(){
-//    console.log('current state is: ', store.getState());
-//});
+import BooksList from './components/pages/booksList';
+
+render(
+    <Provider store={store}>
+        <BooksList />
+    </Provider>,
+    document.getElementById('app')
+);
 
 store.dispatch(postBooks(
     [{
@@ -28,14 +37,14 @@ store.dispatch(postBooks(
     ]
 ));
 
-store.dispatch(deleteBooks(
-    {id: 1}
-));
+//store.dispatch(deleteBooks(
+//    {id: 1}
+//));
 
-store.dispatch(updateBooks(
-    {
-        id: 2,
-        title: 'Learn React in 24h'
-    }
-));
-store.dispatch(addToCart([{id: 1}]));
+//store.dispatch(updateBooks(
+//    {
+//        id: 2,
+//        title: 'Learn React in 24h'
+//    }
+//));
+//store.dispatch(addToCart([{id: 1}]));
