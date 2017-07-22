2d54de7d6dec174e9b76847ce767fb18b56ca8ee
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-22 18:53:08 +0300

    split actions and reducers

--- src/app.js
+++ src/app.js
@@ -1,91 +1,39 @@
 import { createStore } from 'redux';
 
-//STEP 3 define reducers
-const reducer = function(state={books:[]}, action){
-    switch(action.type){
-        case "POST_BOOK":
-            //let books = state.books.concat(action.payload);
-            //return {books};
-            return {books:[...state.books, ...action.payload]};
-            break;
-        case "DELETE_BOOK":
-            // Create a copy of the current array of books
-            const currentBookToDelete = [...state.books];
-            // Determine at which index in books array is the book to be deleted
-            const indexToDelete = currentBookToDelete.findIndex(
-                function(book){
-                    return book.id === action.payload.id;
-                }
-            );
-            //use slice to remove the book at the specified index
-            return {books: [...currentBookToDelete.slice(0, indexToDelete),
-                ...currentBookToDelete.slice(indexToDelete + 1)]};
-                break;
-        case "UPDATE_BOOK":
-            // Create a copy of the current array of books
-            const currentBookToUpdate = [...state.books];
-            // Determine at which index in books array is the book to be deleted
-            const indexToUpdate = currentBookToUpdate.findIndex(
-                function (book) {
-                    return book.id === action.payload.id;
-                }
-            );
-            // Create a new book object with the new values and with the same array index of the item we want
-            // to replace. To achieve this we will use ...spread but we could use concat methods too
-            const newBookToUpdate = {
-                ...currentBookToUpdate[indexToUpdate],
-                title: action.payload.title
-            };
-            // This log has the purpose to show you how newBookToUpdate looks like
-            console.log("what is it newBookToUpdate", newBookToUpdate);
-            // Use slice to remove the book at the specified index, replace with the new object and concatenate with
-            // the rest of item in the array
-            return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate,
-                ...currentBookToUpdate.slice(indexToUpdate + 1)]};
-            break;
-    }
-    return state
-};
+import reducers from './reducers/index';
+import { addToCart } from './actions/cartActions';
+import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
 
-//STEP 1 create the store
-const store = createStore(reducer);
+const store = createStore(reducers);
+
 store.subscribe(function(){
     console.log('current state is: ', store.getState());
-    //console.log('current price: ', store.getState()[1].price);
 });
 
-//STEP 2 create and dispatch actions
-store.dispatch({
-    type:"POST_BOOK",
-    payload: [
-        {
-            id: 1,
-            title: 'this is the book title',
-            description: 'this is the book description',
-            price: 33.33
-        },
-        {
-            id: 2,
-            title: 'this is the second book title',
-            description: 'this is the second book description',
-            price: 50
-        }
-    ]
-});
-
-//DISPATCH a second action
-
-store.dispatch({
-    type:"DELETE_BOOK",
-    payload: {
-        id: 1
+store.dispatch(postBooks(
+    [{
+        id: 1,
+        title: 'this is the book title',
+        description: 'this is the book description',
+        price: 33.33
+    },
+    {
+        id: 2,
+        title: 'this is the second book title',
+        description: 'this is the second book description',
+        price: 50
     }
-});
+    ]
+));
 
-store.dispatch({
-    type: "UPDATE_BOOK",
-    payload: {
+store.dispatch(deleteBooks(
+    {id: 1}
+));
+
+store.dispatch(updateBooks(
+    {
         id: 2,
         title: 'Learn React in 24h'
     }
-});
+));
+store.dispatch(addToCart([{id: 1}]));
