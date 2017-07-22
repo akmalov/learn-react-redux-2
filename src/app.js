dee2b000c74e27ee69159d1b3745e01c197622d9
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-22 17:38:19 +0300

    add to reducer delete and update actions

--- src/app.js
+++ src/app.js
@@ -8,6 +8,41 @@ const reducer = function(state={books:[]}, action){
             //return {books};
             return {books:[...state.books, ...action.payload]};
             break;
+        case "DELETE_BOOK":
+            // Create a copy of the current array of books
+            const currentBookToDelete = [...state.books];
+            // Determine at which index in books array is the book to be deleted
+            const indexToDelete = currentBookToDelete.findIndex(
+                function(book){
+                    return book.id === action.payload.id;
+                }
+            );
+            //use slice to remove the book at the specified index
+            return {books: [...currentBookToDelete.slice(0, indexToDelete),
+                ...currentBookToDelete.slice(indexToDelete + 1)]};
+                break;
+        case "UPDATE_BOOK":
+            // Create a copy of the current array of books
+            const currentBookToUpdate = [...state.books];
+            // Determine at which index in books array is the book to be deleted
+            const indexToUpdate = currentBookToUpdate.findIndex(
+                function (book) {
+                    return book.id === action.payload.id;
+                }
+            );
+            // Create a new book object with the new values and with the same array index of the item we want
+            // to replace. To achieve this we will use ...spread but we could use concat methods too
+            const newBookToUpdate = {
+                ...currentBookToUpdate[indexToUpdate],
+                title: action.payload.title
+            };
+            // This log has the purpose to show you how newBookToUpdate looks like
+            console.log("what is it newBookToUpdate", newBookToUpdate);
+            // Use slice to remove the book at the specified index, replace with the new object and concatenate with
+            // the rest of item in the array
+            return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate,
+                ...currentBookToUpdate.slice(indexToUpdate + 1)]};
+            break;
     }
     return state
 };
@@ -41,11 +76,16 @@ store.dispatch({
 //DISPATCH a second action
 
 store.dispatch({
-    type:"POST_BOOK",
-    payload: [{
-        id: 3,
-        title: 'this is the third book title',
-        description: 'this is the third book description',
-        price: 44
-    }]
+    type:"DELETE_BOOK",
+    payload: {
+        id: 1
+    }
+});
+
+store.dispatch({
+    type: "UPDATE_BOOK",
+    payload: {
+        id: 2,
+        title: 'Learn React in 24h'
+    }
 });
