35566be0f797431e8d5e053edd3086e1e9b51d0e
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-23 16:09:16 +0300

    add booksform

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -5,6 +5,7 @@ import { getBooks } from '../../actions/booksActions';
 import { Grid, Col, Row, Button} from 'react-bootstrap';
 
 import BookItem from './bookItem';
+import BooksForm from './booksForm';
 
 class BooksList extends React.Component{
     componentDidMount(){
@@ -38,7 +39,10 @@ class BooksList extends React.Component{
         });
         return(
             <Grid>
-                <Row style={{marginTop:'15px'}}>
+                <Col sx={12} sm={6}>
+                    <BooksForm/>
+                </Col>
+                <Row>
                     {booksList}
                 </Row>
             </Grid>
