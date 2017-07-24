53042b8c505668e9457db7d009382e7270d13e1e
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-24 13:33:42 +0300

    add modal window to cart

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, Col, Row, Well, Button, ButtonGroup, Label } from 'react-bootstrap';
+import { Modal, Panel, Col, Row, Well, Button, ButtonGroup, Label } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { deleteCartItem, updateCart } from '../../actions/cartActions';
@@ -29,6 +29,20 @@ class Cart extends React.Component {
         if(quantity > 1) {
             this.props.updateCart(_id, -1);
         }
+    }
+
+    constructor(){
+        super();
+        this.state = {
+            showModal:false
+        }
+    }
+
+    open(){
+        this.setState({showModal:true})
+    }
+    close(){
+        this.setState({showModal:false})
     }
 
     render() {
@@ -72,6 +86,29 @@ class Cart extends React.Component {
         return (
             <Panel header="Cart" bsStyle="primary">
                 {cartItemsList}
+                <Row>
+                    <Col>
+                        <h6>Total amount:</h6>
+                        <Button onClick={this.open.bind(this)} bsStyle="success" bsSize="small">
+                            PROCEED TO CHECKOUT
+                        </Button>
+                    </Col>
+                </Row>
+                <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Thank you!</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h6>Your order has been saved</h6>
+                        <p>You will receive an email confirmation</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Col xs={6}>
+                            <h6>Total $</h6>
+                        </Col>
+                        <Button onClick={this.close.bind(this)}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
             </Panel>
         )
     }
