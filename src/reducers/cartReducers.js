529c2d54cc78116206fafa1003445e2fadb2e019
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-24 13:50:31 +0300

    add calculating amount at cart

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,7 +1,11 @@
 export function cartReducers(state={cart:[]}, action) {
     switch(action.type){
         case "ADD_TO_CART":
-            return {...state, cart:action.payload};
+            return {...state,
+                cart:action.payload,
+                totalAmount: totals(action.payload).amount,
+                totalQty: totals(action.payload).qty
+            };
             break;
         case "UPDATE_CART":
             // Create a copy of the current array of books
@@ -18,11 +22,36 @@ export function cartReducers(state={cart:[]}, action) {
             };
             let cartUpdate = [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate,
                 ...currentBookToUpdate.slice(indexToUpdate + 1)];
-            return {...state, cart:cartUpdate};
+            return {...state,
+                cart:cartUpdate,
+                totalAmount: totals(cartUpdate).amount,
+                totalQty: totals(cartUpdate).qty
+            };
             break;
         case "DELETE_CART_ITEM":
-            return {...state, cart:action.payload};
+            return {...state,
+                cart:action.payload,
+                totalAmount: totals(action.payload).amount,
+                totalQty: totals(action.payload).qty
+            };
             break;
     }
     return state;
+}
+
+//CALCULATE TOTAL
+export function totals(payloadArr){
+
+    const totalAmount = payloadArr.map(function(cartArr){
+        return cartArr.price * cartArr.quantity;
+    }).reduce(function(a, b) {
+        return a + b;
+    }, 0); //start summing from index0
+
+    const totalQty = payloadArr.map(function(qty){
+        return qty.quantity;
+    }).reduce(function(a, b) {
+        return a + b;
+    }, 0);
+    return {amount: totalAmount.toFixed(2), qty: totalQty}
 }
