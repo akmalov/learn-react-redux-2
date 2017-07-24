10d0c125d8a882077977912ddd5cd145ecdaf4a4
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-24 13:10:23 +0300

    add update of quantities at cart component

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Panel, Col, Row, Well, Button, ButtonGroup, Label } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { deleteCartItem } from '../../actions/cartActions';
+import { deleteCartItem, updateCart } from '../../actions/cartActions';
 
 class Cart extends React.Component {
 
@@ -21,6 +21,15 @@ class Cart extends React.Component {
         this.props.deleteCartItem(cartAfterDelete);
     };
 
+    onIncrement(_id){
+        this.props.updateCart(_id, 1);
+    }
+
+    onDecrement(_id, quantity){
+        if(quantity > 1) {
+            this.props.updateCart(_id, -1);
+        }
+    }
 
     render() {
         if (this.props.cart[0]) {
@@ -46,12 +55,12 @@ class Cart extends React.Component {
                             <h6>usd. {cartArr.price}</h6>
                         </Col>
                         <Col xs={12} sm={2}>
-                            <h6>qty. <Label bsStyle="success">-</Label></h6>
+                            <h6>qty. <Label bsStyle="success">{cartArr.quantity}</Label></h6>
                         </Col>
                         <Col xs={6} sm={4}>
                             <ButtonGroup style={{minWidth: '300px'}}>
-                                <Button bsStyle="default" bsSize="small">-</Button>
-                                <Button bsStyle="default" bsSize="small">+</Button>
+                                <Button onClick={this.onDecrement.bind(this, cartArr._id, cartArr.quantity)} bsStyle="default" bsSize="small">-</Button>
+                                <Button onClick={this.onIncrement.bind(this, cartArr._id)} bsStyle="default" bsSize="small">+</Button>
                                 <span>    </span>
                                 <Button onClick={this.onDelete.bind(this, cartArr._id)} bsStyle="danger" bsSize="small">Delete</Button>
                             </ButtonGroup>
@@ -74,7 +83,8 @@ function mapStateToProps(state){
 }
 function mapDispatchToProps(dispatch){
     return bindActionCreators({
-        deleteCartItem:deleteCartItem
+        deleteCartItem:deleteCartItem,
+        updateCart:updateCart
     }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
